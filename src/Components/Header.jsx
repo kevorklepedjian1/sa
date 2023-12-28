007a43614b602362d29cea500e357317fee3333c
kevorklepedjian123 <kevork@nelcomdigital.com>
2023-12-28 19:15:17 +0200

    Update header, resume, and index css files (generated by blackbox.ai )

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,51 +1,53 @@
 import React from 'react'
 
 function Header() {
-  return (
-    <>
-    <header class="header">
-            <input class="hamburger" type="checkbox" aria-label="Menu"/>
+    return (
+        <>
+            <header class="header">
+                <input class="hamburger" type="checkbox" aria-label="Menu" />
 
-            <div class="media-header">
-                <span class="media-header__line"></span>
-                <div class="media-header__links">
-                    
-        <a href="https://linkedin.com/in/MunirahD" class="media">
-            <img src="/images/icons/linkedin.svg" alt="linkedin" class="media__icon"/>
-        </a>
-        <a href="mailto:MunirahDu@gmail.com" class="media">
-            <img src="/images/icons/email.svg" alt="email" class="media__icon"/>
-        </a>
-        <a href="https://github.com/MunirahD" class="media">
-            <img src="/images/icons/github.svg" alt="github" class="media__icon"/>
-        </a>
-        <a href="tel:+966545023287" class="media">
-            <img src="/images/icons/phone.svg" alt="phone" class="media__icon"/>
-        </a>
-                </div>
-            </div>
-<div className='king'>
-            <div class="containers">
-                <div class="header__inner">
-                    <a class="logo" href="/">
-                    <img class="logo__img" src="/images/logo.png" alt="Munirah logo"/>
-                        <span class="logo__name">Munirah Alduraibi</span>
-                    </a>
-                    <div class="header__links">
-                        
-                                <a href="/" class="header__link_active header__link ">Home</a>
-                            
-                                <a href="/projects" class="header__link ">Projects</a>
-                            
-                                <a href="/about-me" class="header__link ">About me</a>
-                            
-                        <a href="https://drive.google.com/file/d/1WJXwF1rqWehvEvE5dle9lVV4Zf_ijDM9/view?usp=sharing" class="header__link" target="_blank">CV 🪄</a> 
+                <div class="media-header">
+                    <span class="media-header__line"></span>
+                    <div class="media-header__links">
+
+                        <a href="https://linkedin.com/in/kevork-lepedjian" class="media">
+                            <a href="https://www.linkedin.com/in/kevork-lepedjian" target="_blank" alt="linkedin" class="media__icon"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"></path></svg></a>
+
+                        </a>
+                        <a href="https://github.com/kevorklepedjian1" class="media" >
+                            <a href="https://github.com/kevorklepedjian1" target="_blank" alt="github" class="media__icon"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+                            </a></a>
+                        <a href="mailto:kevorklepedjian1@gmail.com" class="media">
+                            <a href="mailto:kevorklepedjian1@gmail.com" target="_blank" alt="email" class="media__icon"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                            </svg>
+                            </a>
+                        </a>
+
                     </div>
                 </div>
-            </div></div>
-        </header>
-    </>
-  )
+                <div className='king'>
+                    <div class="containers">
+                        <div class="header__inner">
+                            <a class="logo" href="/">
+                                <img class="logo__img" src="/images/logo.png" alt="Munirah logo" />
+                                <span class="logo__name">Munirah Alduraibi</span>
+                            </a>
+                            <div class="header__links">
+
+                                <a href="/" class="header__link_active header__link ">Home</a>
+
+                                <a href="/projects" class="header__link ">Projects</a>
+
+                                <a href="/about-me" class="header__link ">About me</a>
+
+                                <a class="header__link" >Contact me</a>
+                            </div>
+                        </div>
+                    </div></div>
+            </header>
+        </>
+    )
 }
 
 export default Header
--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -6,18 +6,25 @@ import React from 'react';
 const SkillsBlock = () => {
   const skills = [
     { name: 'HTML', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/31f11147f89dbd855a9b948e2ce643ea2f41c0a9-480x480.png' },
-    { name: 'CSS', imageSrc: 'css_image_url.jpg' },
-    { name: 'JavaScript', imageSrc: 'js_image_url.jpg' },
-    { name: 'Node.js', imageSrc: 'nodejs_image_url.jpg' },
-    { name: 'Tailwind CSS', imageSrc: 'tailwind_image_url.jpg' },
-    { name: 'Next.js', imageSrc: 'nextjs_image_url.jpg' },
-    { name: 'React Native', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/34be54263e18fa2aea611c6b3b388b76e978e7d7-64x64.png' },
+    { name: 'CSS', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/107574d887f1c07c9fbea0704de7f293e244a8f3-480x480.png' },
+    { name: 'JavaScript', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/66bbf9242b1ccffebb5d46f376f5036b527fad48-480x480.png' },
+    { name: 'TypeScript', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/64f3e20f8ad906d56dea96863b6db87fad6efb52-64x64.png' },
+    { name: 'Node.js', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/d8c8025b1695a3f14f849b99afc71d917ef40813-480x480.png' },
+    { name: 'Express.js', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/5f27ecd489cbd1cf6bdf703f6514cf4cb59638db-500x500.png' },
+    { name: 'Tailwind', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/b945eb4f577059132d07f35ac087ce31cb092ae1-431x290.png' },
+    { name: 'Next.js', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/7f3a652dd4e48f795856f28bd56d120bda77c30a-1024x1024.svg' },
+    { name: 'Mongodb', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/6c9be215123947cde5182d5240a777adca4d99e0-120x258.svg' },
+    { name: 'Framer', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/8e8b514518628b3bff60eab330b1982adcbc3382-2500x2500.svg' },
+    { name: 'React/RN', imageSrc: 'https://cdn.sanity.io/images/ed7e5ihf/production/34be54263e18fa2aea611c6b3b388b76e978e7d7-64x64.png' },
     // Add more skills as needed
   ];
 
   return (
     <div className="block-container">
-      <h2>Skills</h2>
+      <div class="projects__header">
+                <h2 class="h2">Projects</h2>
+             
+            </div>
       <div className="skills-container">
         {skills.map((skill) => (
           <div key={skill.name} >
@@ -34,18 +41,36 @@ const SkillsBlock = () => {
 
 const ExperienceBlock = () => {
   const experiences = [
-    { company: 'Company A', position: 'Frontend Developer', duration: 'Jan 2020 - Mar 2022' },
-    { company: 'Company B', position: 'Full Stack Developer', duration: 'Apr 2022 - Present' },
+    {
+      company: 'Freelancer',
+      position: 'Frontend Developer',
+      duration: '2021 - Present',
+      description:
+        'As a freelance Frontend Developer, I work on a variety of projects, delivering bespoke web applications. ' +
+        'I specialize in using React.js and Redux for state management, implementing Higher Order Components (HOCs) ' +
+        'for code reusability, and utilizing Tailwind CSS for efficient and responsive styling.'
+    },
+    {
+      company: 'Nelcom Digital',
+      position: 'Frontend Developer',
+      duration: 'Feb 2023 - Jul 2023',
+      description:
+        'At Nelcom Digital, I contributed to the development of cutting-edge web applications. ' +
+        'I leveraged React.js for building dynamic user interfaces, utilized Node.js and Express.js for server-side ' +
+        'development, and incorporated Framer Motion to create smooth and interactive animations.'
+    },
     // Add more experiences as needed
   ];
 
   return (
     <div className="block-container">
+      <div class="projects__header" style={{marginTop:"40px"}}></div>
       <h2>Work Experience</h2>
       <ul className="experience-list">
         {experiences.map((experience) => (
           <li key={experience.company}>
             <strong>{experience.position}</strong> at {experience.company}, {experience.duration}
+            <p>{experience.description}</p>
           </li>
         ))}
       </ul>
@@ -57,6 +82,7 @@ const Resume = () => {
   return (
     <div className='king'>
     <div className="resume-container">
+      
       <SkillsBlock />
       <ExperienceBlock />
     </div>
