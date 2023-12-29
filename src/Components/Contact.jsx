a8f6c98b47d39ae7ef507a30aab9274686fd0885
unknown <kevorklepedjian1@gmail.com>
2023-12-30 00:34:37 +0200

    first commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='lp'><h1 class="hero__title">👉<a href='mailto:kevorklepedjian1@gmail.com'>Kevorklepedjian1@gmail.com</a>👈 </h1></div>
+    <div className='king'>
+    <sections class="contacts" id="contacts">
+    <div class="projects__header">
+                <h2 class="h2">Contact Me</h2>
+               
+            </div>
+    <div class="contacts__content">
+        <p class="contacts__description">I'm always <span>interested in backend and/or frontend development</span>, whether it's for work or freelance opportunities. However, if you have other requests or questions, please don't hesitate to contact me.</p>
+        <div class="contacts__media">
+            <h3 class="contacts__title">Contact me here</h3>
+            <div class="contacts__list">
+                
+                    <div class="contact-item">
+                        
+                    <a href="mailto:kevorklepedjian1@gmail.com" class="media">
+                            <a href="mailto:kevorklepedjian1@gmail.com" target="_blank" alt="email" class="media__icon"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                            </svg>
+                            </a>
+                        </a>
+                        <span class="contact-text">kevorklepedjian1@gmail.com</span>
+                    </div>
+                
+                    
+                
+            </div>
+        </div>
+    </div>
+</sections>
+</div>
   )
 }
 
